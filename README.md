a36c7ff2a608fd172d7d7f67ffccbc3cdbeb7d47
magic7s <brad@magic7s.com>
2021-11-18 18:34:11 +0000

    Fist Commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# expose-jwt-action
-View GitHub JWT Token
+# View GitHub JWT Token
+
+This action prints "Hello World" or "Hello" + the name of a person to greet to the log.
+
+## Inputs
+
+## `aud`
+
+The audiance field in the JWT
+
+Default: `FOO.BAR`
+
+## Outputs
+
+## `jwt`
+
+The JSON Web Token signed by GitHub Actions
+
+## Example usage
+
+uses: magic7s/expose-jwt-action@main
+with:
+  aud: 'sts.amazonaws.com'
---
+++ index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // Get aud and request token
+  const aud = core.getInput('aud');
+  jwt = await core.getIDToken(aud);
+  core.setOutput("jwt", jwt);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
